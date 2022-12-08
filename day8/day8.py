5d43d010240474c7279f71d637720ee44174223c
paloblanco <45581243+paloblanco@users.noreply.github.com>
2022-12-08 08:55:09 -0500

    day 8 complete

--- day8/day8.py
+++ day8/day8.py
@@ -54,11 +54,58 @@ def sum_of_visible_trees(forest: list[list[int]]) -> int:
         rolling_sum += sum(row)
     return rolling_sum
 
+def get_score_in_direction(view: list[int]) -> int:
+    if len(view)==1: 
+        return 0
+    my_tree = view[0]
+    score=0
+    for tree in view[1:]:
+        score += 1
+        if tree >= my_tree:
+            break
+    return score
+
+def get_score_for_location(forest: list[list[int]], r: int, c: int) -> int:
+    left_look = forest[r][c::-1]
+    left_score = get_score_in_direction(left_look)
+
+    right_look = forest[r][c::]
+    right_score = get_score_in_direction(right_look)
+
+    up_look = [forest[row_ix][c] for row_ix in range(r,-1,-1)]
+    up_score = get_score_in_direction(up_look)
+
+    height = len(forest)
+    down_look = [forest[row_ix][c] for row_ix in range(r,height)]
+    down_score = get_score_in_direction(down_look)
+
+    score = left_score*right_score*up_score*down_score
+    return score
+
+def create_map_of_scenic_scores(forest: list[list[int]]) -> list[list[int]]:
+    height = len(forest)
+    width = len(forest[0])
+    forest_scores = [[0 for w in range(width)] for h in range(height)]
+    
+    for r in range(height):
+        for c in range(width):
+            score = get_score_for_location(forest,r,c)
+            forest_scores[r][c] = score
+    return forest_scores
+
+def get_max_score_in_forest(forest_scores: list[list[int]]) -> int:
+    max_current = 0
+    for row in forest_scores:
+        max_current = max(row + [max_current,])
+    return max_current
 
 if __name__ == "__main__":
     forest = return_list_of_lists_from_file()
     forest_bin = create_map_of_visibile_trees(forest)
     visible_count = sum_of_visible_trees(forest_bin)
+    forest_scores = create_map_of_scenic_scores(forest)
+    max_score = get_max_score_in_forest(forest_scores)
     print(f"Total trees: {len(forest) * len(forest[0])}")
     print(f"Visible tree count: {visible_count}")
+    print(f"Max score: {max_score}")
 
