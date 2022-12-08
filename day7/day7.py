9d4dc59cdad5ecae97fd890ae5a78ad47fc38782
paloblanco <panella.rocco@gmail.com>
2022-12-08 07:59:35 -0500

    quitting pretty print on day7

--- day7/day7.py
+++ day7/day7.py
@@ -23,6 +23,27 @@ class Directory(Node):
         self.children.append(child)
         child.parent = self
 
+    def get_all_extra_info(self):
+        self.size = self.calc_size()
+        self.width = self.calc_width()
+        self.depth = self.calc_depth()
+        self.position = self.get_position()
+        for child in self.children:
+            if type(child) != Directory: continue
+            child.get_all_extra_info()
+        self.max_depth = self.calc_max_depth()
+
+    def get_position(self):
+        position=0
+        if self.parent:
+            position = self.parent.children.index(self)
+            if self.parent.position:
+                position += self.parent.position
+            else:
+                position += self.parent.get_position()
+        return position
+
+
     def calc_size(self):
         size = 0
         for child in self.children:
@@ -32,6 +53,30 @@ class Directory(Node):
                 size += child.calc_size()
         return size
 
+    def calc_width(self):
+        width = 0
+        child_dirs = 0
+        for child in self.children:
+            if type(child) != Directory: continue
+            width += child.calc_width()
+        if width==0: return 1
+        return width
+
+    def calc_max_depth(self):
+        # must come after depth calc
+        max_depth = self.depth
+        for child in self.children:
+            if type(child) != Directory: continue
+            max_depth = max(max_depth,child.depth)
+        return max_depth
+
+    def calc_depth(self):
+        if not self.parent:
+            return 0
+        depth=1
+        depth += self.parent.calc_depth()
+        return depth
+
     def check_if_child_exists(self,name):
         for child in self.children:
             if name == child.name:
@@ -39,12 +84,24 @@ class Directory(Node):
         return False
 
     def __str__(self):
-        return f"- Dir: {self.name}"
+        # return f"- Dir: {self.name}"
+        return self.name[0]
 
     def __repr__(self):
         return self.__str__()
 
-    def pprint(self, indent=0):
+    def pretty_print(self, indent: str = '', last_child: bool = True) -> None:
+        # Print the current node
+        print(indent + ('└' if last_child else '├') + ' ' + str(self))
+        
+        # If the node has children, print them each on their own line with a
+        #  deeper indentation level
+        if self.children:
+            for i, child in enumerate(self.children):
+                if type(child) != Directory: continue
+                child.pretty_print(indent + ('   ' if last_child else '│  '), i == len(self.children) - 1)
+    
+    def pprint_old(self, indent=0):
         structure=f"{'  '*indent}{self} :"
         children_dir = []
         children_file = []
@@ -58,7 +115,7 @@ class Directory(Node):
         for child in children_dir:
             structure += "\n"
             structure += child.pprint(indent=indent+1)
-        return structure, indents
+        return structure
 
 
 class File(Node):
@@ -111,6 +168,7 @@ def create_graph_from_text(lines,draw=False):
                 current_node.add_child(File(child_name, size))
         if draw:
             clear()
+            root.get_all_extra_info()
             print(root.pprint())
             sleep(0.01)
     return root
@@ -165,9 +223,10 @@ def test_graph():
     print(a.calc_size())
 
 if __name__ == "__main__":
-    # running_sum = get_sum_of_small_directories()
-    # print(f"Sum small directories: {running_sum}")
-    # smallest_directory_size_to_delete = smallest_directory_to_delete()
-    # print(f"Smallest directory size to delete: {smallest_directory_size_to_delete}")
-    lines = get_inputs_from_file()
-    root = create_graph_from_text(lines,draw=True)
+    running_sum = get_sum_of_small_directories()
+    print(f"Sum small directories: {running_sum}")
+    smallest_directory_size_to_delete = smallest_directory_to_delete()
+    print(f"Smallest directory size to delete: {smallest_directory_size_to_delete}")
+    # lines = get_inputs_from_file()
+    # root = create_graph_from_text(lines,draw=False)
+    # root.pretty_print()
