a779f0bf01130ab7d0ff000768816eefab6785bb
paloblanco <45581243+paloblanco@users.noreply.github.com>
2022-12-12 13:31:44 -0500

    part 2 done with bfs

--- day12/day12.py
+++ day12/day12.py
@@ -17,8 +17,21 @@ class Node:
     to_me: 'Node' = None # path taken to me
 
     @property
+    def steps_to_me(self):
+        steps_here = 0
+        parent = self.to_me
+        while parent:
+            steps_here+=1
+            parent=parent.to_me
+        return steps_here
+
+    @property
+    def index(self):
+        return (self.row,self.column)
+
+    @property
     def weight(self):
-        return self.heuristic(self.row,self.column)
+        return self.heuristic(self.row,self.column, self.steps_to_me)
 
     def __lt__(self, other):
         return self.weight < other.weight
@@ -46,7 +59,6 @@ class PQueue:
 
 @dataclass
 class Queue:
-
     _container: deque[Node] = field(default_factory=deque)
 
     def push(self, node: Node):
@@ -64,12 +76,12 @@ class MapGraph:
 
     def __post_init__(self):
         self.nodes = []
-        heuristic = return_heuristic_calc(self.start, self.end)
+        self.heuristic = return_heuristic_calc(self.start, self.end)
         # make map of nodes
         for r,row in enumerate(self._map):
             node_row = []
             for c,height in enumerate(row):
-                node_here = Node(height, r, c, heuristic)
+                node_here = Node(height, r, c, self.heuristic)
                 node_row.append(node_here)
             self.nodes.append(node_row)
         # connect nodes
@@ -87,29 +99,45 @@ class MapGraph:
         for row in self._map:
             print(''.join([chr(each) for each in row]))
 
+    def make_node_from_map(self, row,col):
+        height = self._map[row][col]
+        return Node(height,row,col,self.heuristic)
+
+    def get_next_steps(self,current_node):
+        r,c = current_node.index
+        possible_steps = []
+        if r > 0 and self._map[r-1][c] <= current_node.height + 1:
+            possible_steps.append(Node(self._map[r-1][c],r-1,c,self.heuristic))
+        if r < len(self._map)-1 and self._map[r+1][c] <= current_node.height + 1:
+            possible_steps.append(Node(self._map[r+1][c],r+1,c,self.heuristic))
+        if c > 0 and self._map[r][c-1] <= current_node.height + 1:
+            possible_steps.append(Node(self._map[r][c-1],r,c-1,self.heuristic))
+        if c < len(self._map[0])-1 and self._map[r][c+1] <= current_node.height + 1:
+            possible_steps.append(Node(self._map[r][c+1],r,c+1,self.heuristic))
+        return possible_steps
+    
     def astar(self):
         frontier = Queue()
-        explored = []
+        explored = set()
         self.path = []
-        start_node = self.nodes[self.start[0]][self.start[1]]
-        explored.append(start_node)
-        end_node = self.nodes[self.end[0]][self.end[1]]
+        start_node = self.make_node_from_map(self.start[0],self.start[1])#self.nodes[self.start[0]][self.start[1]]
+        explored.add(start_node.index)
+        # end_node = self.nodes[self.end[0]][self.end[1]]
         current_node = start_node
-        for node in current_node.next_steps:
+        for node in self.get_next_steps(current_node):
             frontier.push(node)
             node.to_me = current_node
-            explored.append(node)
+            explored.add(node.index)
         while frontier._container:
             current_node = frontier.pop()
-            if current_node is end_node:
-                print("success")
+            if current_node.index == self.end:
                 return current_node
             else:
-                for node in current_node.next_steps:
-                    if node in explored: continue
+                for node in self.get_next_steps(current_node):
+                    if node.index in explored: continue
                     frontier.push(node)
                     node.to_me = current_node
-                    explored.append(node)
+                    explored.add(node.index)
         return None
 
     def _print_solution(self, last_node: Node):
@@ -144,10 +172,11 @@ def return_heuristic_calc(start: tuple[int,int],end: tuple[int,int]) -> callable
     startcol = start[1]
     endrow = end[0]
     endcol = end[1]
-    def heuristic_calc(row,column) -> int:
+    def heuristic_calc(row,column,steps) -> int:
         manhattan_0 = abs(row - startrow) + abs(column - startcol)
         manhattan_1 = abs(endrow - row) + abs(endcol - column)
-        return manhattan_0 + manhattan_1    
+        # return manhattan_0 + manhattan_1    
+        return steps #+ manhattan_1    
     return heuristic_calc
 
 
@@ -169,18 +198,40 @@ def return_map_from_file(fname=TEST_NAME):
         map.append(row)
     return map, start, end
 
+def part_1(fname=TEST_NAME):
+    map, start, end = return_map_from_file(fname)
+    mapg = MapGraph(map,start,end)
+    steps = mapg.solve_problem(show=True)
+    return steps
 
+def part_2(fname=TEST_NAME):
+    map, _s, end = return_map_from_file(fname)
+    possible_starts = []
+    for r,row in enumerate(map):
+        for c,val in enumerate(row):
+            if val==ord('a'):
+                possible_starts.append((r,c))
+    solutions = []
+    for start in possible_starts:
+        mapg = MapGraph(map,start,end)
+        steps = mapg.solve_problem(show=False)
+        if steps:
+            solutions.append(steps)
+    return min(solutions)
 
 
 if __name__ == "__main__":
-    map_test, start_test, end_test = return_map_from_file()
-    mapg_test = MapGraph(map_test,start_test,end_test)
-    steps_test = mapg_test.solve_problem(show=True)
-    assert steps_test == 31
+    assert part_1() == 31
 
-    map, start, end = return_map_from_file(INPUT_NAME)
-    mapg = MapGraph(map,start,end)
-    steps = mapg.solve_problem(show=True)
-    print(f"Part 1: {steps =}")
+    steps_1 = part_1(INPUT_NAME)
+    print(f"{steps_1 =}")
+
+    part2test = part_2()
+    assert part2test == 29
+
+    steps_2 = part_2(INPUT_NAME)
+    print(f"{steps_2 =}")
+
+    
 
 
