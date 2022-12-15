e2a1833619edbb8b7d55f8409f7bc6933fd142a0
paloblanco <panella.rocco@gmail.com>
2022-12-15 11:10:23 -0500

    part1 day15 done

--- day15/day15.py
+++ day15/day15.py
@@ -22,65 +22,41 @@ def return_tuples_from_file(fname=TEST_NAME):
     b = [min(bx),min(by),max(bx),max(by)]
     return tuple_list, b
 
-EMPTY   = 0
-SENSOR  = 1
-BEACON  = 2
-EXCLUDE = 3
-
-def get_tuples_in_range(x: int,y: int,d: int):
-    if d == 0: return []
-    tups = []
-    for yy in range(y-d,y+d+1):
-        dx = d-abs(yy-y)
-        xx = x-dx
-        tups.append((xx,yy,2*dx+1))
-    return tups
-
-
-def make_exclusion_map_from_data(tuple_list: list[list[tuple[int,int]]]):
-    map = defaultdict(int)
-    minx = []
-    for (xs,ys), (xb,yb) in tuple_list:
-        # map[(xs,ys)] = SENSOR
+def return_sensors_with_radii(t):
+    sensor_radii = defaultdict(int)
+    for (xs,ys), (xb,yb) in t:
         man_dist = abs(xs-xb) + abs(ys-yb)
-        tups_exclude = get_tuples_in_range(xs,ys,man_dist)
-        for xe,ye,d in tups_exclude:
-            map[(xe,ye)] = d
-            minx.append(xe)
-    return map, min(minx)
+        sensor_radii[(xs,ys)] = man_dist
+    return sensor_radii
+
+def collide(point,s_r):
+    x = point[0]
+    y = point[1]
+    for (xs,ys), d in s_r.items():
+        dd = abs(x-xs) + abs(y-ys)
+        if dd <= d:
+            return True
+    return False
 
 def part1(fname=TEST_NAME, ytarget: int = 10):
     t,b = return_tuples_from_file(fname)
     sensors = {(each[0][0],each[0][1]) for each in t}
     beacons = {(each[1][0],each[1][1]) for each in t}
-    map,minx = make_exclusion_map_from_data(t)
     exclude_count = 0
-    str_row = ""
-    x_range_build = range(minx,b[2]+1) 
-    x_range_solve = range(b[0],b[2]+1) 
-    thisrow = defaultdict(int)
-    for x in x_range_build:
-        val = map[(x,ytarget)]
-        if val == 0: 
-            continue
-        for i in range(val):
-            thisrow[x+i]=1
-        print(f"{x=}  {val=}")
-    row = ""
-    for x in x_range_solve: 
-        row += str(thisrow[x])
-    print(row)
-    for x in x_range_solve:
-        if (x,ytarget) not in beacons:
-           exclude_count+=thisrow[x] 
-        else:
-            print("beacon!")
+    sensors_with_radii = return_sensors_with_radii(t)
+    maxd = max([d for d in sensors_with_radii.values()])
+    minx = b[0] - maxd
+    maxx = b[2] + maxd
+    for x in range(minx,maxx+1):
+        point = (x,ytarget)
+        if (collide(point,sensors_with_radii)) and (not point in beacons):
+            exclude_count += 1
     return exclude_count
-
+    
 
 if __name__ == "__main__":
     test1 = part1()
     print(f"{test1 =}")
 
-    # part1_solve = part1(INPUT_NAME, ytarget=2000000)
-    # print(f"{part1_solve =}")
+    part1_solve = part1(INPUT_NAME, ytarget=2000000)
+    print(f"{part1_solve =}") # 4254101 too low
