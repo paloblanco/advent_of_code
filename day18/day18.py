eafdee476033754c6b0d2d49569193b87f5b88ed
paloblanco <45581243+paloblanco@users.noreply.github.com>
2022-12-19 13:43:18 -0500

    part2 right

--- day18/day18.py
+++ day18/day18.py
@@ -31,37 +31,37 @@ def part2(fname=TEST_NAME):
     lenx = max([x for x,y,z in cubes])
     leny = max([y for x,y,z in cubes])
     lenz = max([z for x,y,z in cubes])
+    minx,miny,minz = -1,-1,-1
     box_size = (lenx+2)*(leny+2)*(lenz+2)
 
     # comb outer space, count blocks we can reach
     frontier = []
     explored = set()
-    startcube = (0,0,0)
+    startcube = (minx,miny,minz)
     frontier.append(startcube)
     explored.add(startcube)
+    sides_touched = 0
     while frontier:
         x,y,z = frontier.pop()
-        for dx in [-1,1]:
-            if x+dx < 0 or x+dx > lenx+2: 
+        neighbors = [
+            [x+1,y,z],
+            [x-1,y,z],
+            [x,y+1,z],
+            [x,y-1,z],
+            [x,y,z+1],
+            [x,y,z-1],
+            ]
+        for xnew,ynew,znew in neighbors:
+            if xnew < minx or xnew > lenx+2: continue
+            if ynew < miny or ynew > leny+2: continue
+            if znew < minz or znew > lenz+2: continue
+            if (xnew,ynew,znew) in cubes: sides_touched += 1
+            if (xnew,ynew,znew) in cubes or (xnew,ynew,znew) in explored:
                 continue
-            if (x+dx,y,z) in cubes or (x+dx,y,z) in explored:
-                continue
-            explored.add((x+dx,y,z))
-            frontier.append((x+dx,y,z))
-        for dy in [-1,1]:
-            if y+dy < 0 or y+dy > leny+2: 
-                continue
-            if (x,y+dy,z) in cubes or (x,y+dy,z) in explored:
-                continue
-            explored.add((x,y+dy,z))
-            frontier.append((x,y+dy,z))
-        for dz in [-1,1]:
-            if z+dz < 0 or z+dz > lenz+2: 
-                continue
-            if (x,y,z+dz) in cubes or (x,y,z+dz) in explored:
-                continue
-            explored.add((x,y,z+dz))
-            frontier.append((x,y,z+dz))
+            explored.add((xnew,ynew,znew))
+            frontier.append((xnew,ynew,znew))
+    print(f"{sides_touched=}")
+
     outer_bubble = len(explored)
     
     pockets = set()
@@ -71,29 +71,38 @@ def part2(fname=TEST_NAME):
         for dx in [-1,1]:
             if (x+dx,y,z) in cubes:
                 adjacent += 1
-            elif (x+dx,y,z) not in explored:
+            else:
                 pockets.add((x+dx,y,z))
         for dy in [-1,1]:
             if (x,y+dy,z) in cubes:
                 adjacent += 1
-            elif (x,y+dy,z) not in explored:
+            else:
                 pockets.add((x,y+dy,z))
         for dz in [-1,1]:
             if (x,y,z+dz) in cubes:
                 adjacent += 1
-            elif (x,y,z+dz) not in explored:
+            else:
                 pockets.add((x,y,z+dz))
         sides += 6-adjacent
-
-    print(f"{pockets=}")
+    print(f"{sides=}")
+    pockets = pockets - explored
+    # print(f"{pockets=}")
+    print(f"{pockets & cubes=}")
+    print(f"{pockets & explored=}")
+    adjacent_solids = 0
     for x,y,z in pockets:
-        adjacent_solids = 0
-        for dx in [-1,0,1]:
-            for dy in [-1,0,1]:
-                for dz in [-1,0,1]:
-                    if (x+dx,y+dy,z+dz) in cubes:
-                        # print((x+dx,y+dy,z+dz))
-                        adjacent_solids += 1
+        for dx in [-1,1]:
+            if (x+dx,y,z) in cubes:
+                # print("solid")
+                adjacent_solids += 1
+        for dy in [-1,1]:
+            if (x,y+dy,z) in cubes:
+                # print("solid")
+                adjacent_solids += 1
+        for dz in [-1,1]:
+            if (x,y,z+dz) in cubes:
+                # print("solid")
+                adjacent_solids += 1
     return sides - adjacent_solids
 
 if __name__ == "__main__":
@@ -107,5 +116,5 @@ if __name__ == "__main__":
     print(f"{test2=}")
 
     real2 = part2(INPUT_NAME)
-    print(f"{real2=}")
+    print(f"{real2=}") # 4504 too high, 2549 too low
     
