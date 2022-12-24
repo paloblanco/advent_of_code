beea698a1f2aa2b714da900480a0bfe8fa39c2af
paloblanco <panella.rocco@gmail.com>
2022-12-24 13:55:48 -0500

    working on dy>0

--- day22/day22.py
+++ day22/day22.py
@@ -103,7 +103,107 @@ def part1(fname=TEST_NAME):
     print(f"{x=}   {y=}   {direction=}")
     return 4*x + 1000*y + direction
 
+ZONES = [
+    [0,1,2],
+    [0,3,0],
+    [4,5,0],
+    [6,0,0]
+]
 
+def get_zone(x,y):
+    return ZONES[(y-1)//50][(x-1)//50]
+
+def part2(fname=TEST_NAME):
+    map = Map(fname)
+    x,y = 1,1
+    while map.mget(x,y) != 1:
+        x += 1
+    direction = 0
+    instructions = map._directions
+    for step in instructions:
+        if step=="L":
+            direction = (direction - 1)%4
+        elif step=="R":
+            direction = (direction + 1)%4
+        else:
+            for i in range(step):
+                zone_now = get_zone(x,y)
+                dx,dy = DIRECTIONS[direction]
+                direction_old=direction
+                nx,ny = x+dx,y+dy
+                #wrap if needed
+                if dx > 0:
+                    if nx > map.width or map.mget(nx,ny)==0:
+                        match zone_now:
+                            case 2:
+                                nx=100
+                                direction=2
+                                ny=100+(51-ny)
+                            case 3:
+                                ny=50
+                                direction=3
+                                nx=100+(ny-50) 
+                            case 5:
+                                nx=150
+                                direction=2
+                                ny=151-ny
+                            case 6:
+                                ny=150
+                                direction=3
+                                nx=50+(ny-150)
+                elif dy < 0:
+                    if nx < 1 or map.mget(nx,ny)==0:
+                        match zone_now:
+                            case 1:
+                                nx=1
+                                direction=0
+                                ny=100+(51-ny)
+                            case 3:
+                                ny=101
+                                direction=1
+                                nx = ny-50
+                            case 4:
+                                nx=51
+                                direction=0
+                                ny=151-ny
+                            case 6:
+                                nx=1
+                                direction=1
+                                nx = ny-100
+                elif dy > 0:
+                    if ny > map.height or map.mget(nx,ny)==0:
+                        match zone_now:
+                            case 2:
+                                nx=100
+                                direction=2
+                                ny=100+(51-ny)
+                            case 5:
+                                ny=50
+                                direction=3
+                                nx=100+(ny-50) 
+                            case 6:
+                                nx=150
+                                direction=2
+                                ny=151-ny
+                            
+
+
+
+                if nx > map.width: nx = 1
+                if nx < 1: nx = map.width
+                if ny > map.height: ny = 1
+                if ny < 1: ny = map.height
+                while map.mget(nx,ny)==0:
+                    nx,ny = nx+dx,ny+dy
+                    if nx > map.width: nx = 1
+                    if nx < 1: nx = map.width
+                    if ny > map.height: ny = 1
+                    if ny < 1: ny = map.height
+                if map.mget(nx,ny)==2:
+                    break
+                x,y = nx,ny
+    print(f"{x=}   {y=}   {direction=}")
+    return 4*x + 1000*y + direction
 
 
 if __name__ == "__main__":
