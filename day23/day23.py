bc9257f8ac94ec3fe3b6e8aef00017fe4f8dc296
paloblanco <panella.rocco@gmail.com>
2022-12-29 14:58:16 -0500

    p1 23 passing

--- day23/day23.py
+++ day23/day23.py
@@ -21,20 +21,39 @@ DIRECTIONS = [
     [0,1],
 ]
 
+def print_set(elfmap):
+    miny,maxy,minx,maxx = 0,0,0,0
+    for (y,x) in elfmap:
+        miny = min([miny,y])
+        maxy = max([maxy,y])
+        minx = min([minx,x])
+        maxx = max([maxx,x])
+    h = maxy-miny + 1
+    w = maxx-minx + 1
+    pmap = [["." for e in range(w)] for i in range(h)]
+    for (y,x) in elfmap:
+        pmap[y-miny][x-minx] = "#"
+    print(f"{miny=}   {minx=}")
+    for r in pmap:
+        print(''.join(r))
+
+
 def part1(fname=TEST_NAME,turns=10):
     elf_map = read_input_map(fname)
     direction_turn = 0
     miny,maxy,minx,maxx = 0,0,0,0
+    # print("INITIAL")
+    # print_set(elf_map)
     for t in range(turns):
         # consideration
         to_map = defaultdict(int)
         dir_map = {}
         for elf in elf_map:
             around_me = {(elf[0]+y,elf[1]+x) for y,x in product([-1,0,1],repeat=2)}
-            if len(around_me & elf_map) < 1: 
+            if len(around_me & elf_map) < 2: 
                 continue # noone else is around me, so break
             for d in range(4):
-                direction = (d+direction_turn)%4 #changes each turn
+                direction = (d + direction_turn)%4 #changes each turn
                 if direction//2==0: 
                     directions_add = [[0,-1],[0,0],[0,1]]
                 else:
@@ -48,10 +67,8 @@ def part1(fname=TEST_NAME,turns=10):
                     dir_map[elf] = direction
                     break # get out of this inner direction loop
         # if to_map is more than 1 anywhere, those elves don't move
-        for (y,x) in elf_map:
-            if (y,x) not in dir_map: 
-                continue #break out if this elf is not moving
-            yplus,xplus = DIRECTIONS[dir_map[(y,x)]]
+        for (y,x),direction in dir_map.items():
+            yplus,xplus = DIRECTIONS[direction]
             yto,xto = y+yplus,x+xplus
             if to_map[(yto,xto)] > 1:
                 continue # more than one elf wants to go here
@@ -64,14 +81,18 @@ def part1(fname=TEST_NAME,turns=10):
         direction_turn = (direction_turn+1)%4 #rotate through directions
     # get area
     area = (maxy-miny+1) * (maxx-minx+1)
-    print(f"{area=}")
-    print(f"{maxy=}   {miny=}   {maxx=}   {minx=}")
+    # print(f"{area=}")
+    # print(f"{maxy=}   {miny=}   {maxx=}   {minx=}")
+    # print_set(elf_map)
     return area - len(elf_map)
     
 
 if __name__ == "__main__":
     test1 = part1()
     print(f"{test1=}")
+
+    p1 = part1(INPUT_NAME)
+    print(f"{p1=}") #4109 too high
 
             
 
